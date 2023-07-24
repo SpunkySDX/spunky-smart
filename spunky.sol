22f7b504d2d78e88af3b9aa2bc9ac51b390a3904
Meshach Ishaya <meshachsatoshi1@gmail.com>
2023-07-24 12:29:09 +0100

    Update spunky.sol

--- spunky.sol
+++ spunky.sol
@@ -30,10 +30,10 @@ contract SpunkySDX is Ownable {
     mapping(address => uint256) private _stakingRewards;
 
     // Token distribution details
-    uint256 private WHITELIST_ALLOCATION = 0; // 2% of total supply
-    uint256 private PRESALE_ALLOCATION = 0; // 20% of total supply
-    uint256 private IEO_ALLOCATION = 0; // 8% of total supply 
-    uint256 private AIRDROP_ALLOCATION = 0; // 8% of total supply 
+    uint256 private WHITELIST_ALLOCATION = 0; 
+    uint256 private PRESALE_ALLOCATION = 0; 
+    uint256 private IEO_ALLOCATION = 0; 
+    uint256 private AIRDROP_ALLOCATION = 0; 
 
     // Slippage tolerance
     uint256 private constant MAX_SLIPPAGE_TOLERANCE = 5;
